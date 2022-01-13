6e4353db0228b06502f0ae29f267abc38fdbf06e
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-13 16:39:11 -0300

    Cors able

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 
+const allowCors = require('./cors');
+
 server.use(bodyParser.urlencoded({
-  extended: true 
+  extended: true
 }));
-server.use(bodyParser.json());
+server.use(bodyParser.json())
+server.use(allowCors)
 
+server.listen(port, function(){
+    console.log(`Backend is running on port ${port}.`)
+})
 
-server.listen(port, function() {
-  console.log(`Backend is running on port ${port}.`);
-});
-
-module.exports = server;
+module.exports = server

6ec3a75175994147158c31b4b271a0c9cd05f408
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-14 15:11:21 -0300

    Created events Add/onChange

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -5,6 +5,32 @@ import TodoList from "./TodoList";
 
 export default class Todo extends Component {
 
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      description: '',
+      list: []
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(){
+    console.log(this.state.description);
+  }
+
+  handleChange(e){
+    this.setState(
+      {
+        ...this.state, 
+        description: e.target.value
+      }
+    );
+  }
+
   render(){
     return (
 
@@ -13,7 +39,11 @@ export default class Todo extends Component {
           title='Todo' 
           small='Register' 
         />
-        <TodoForm />
+        <TodoForm 
+          description={this.state.description}
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}
+        />
         <TodoList />
       </div>
 

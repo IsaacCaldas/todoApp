6b44022bfcbd4f37a4c29bd6a3aebfd1502d34e9
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-14 15:17:43 -0300

    Add event - integrated frontend / backend

--- frontend/src/todo/Todo.jsx
+++ frontend/src/todo/Todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
 import PageHeader from "../template/PageHeader";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
@@ -19,7 +23,11 @@ export default class Todo extends Component {
   }
 
   handleAdd(){
-    console.log(this.state.description);
+    const description = this.state.description;
+
+    axios.post(URL, { 
+      description 
+    }).then(resp => console.log('e'));
   }
 
   handleChange(e){

466464970371fff1a50fdee7c47a950b40f98744
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-14 13:25:26 -0300

    Created Todo/About/Menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,15 +1,20 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'; 
 import 'modules/font-awesome/css/font-awesome.min.css';
-import '../styles/app.css'
+import '../styles/app.css';
 
 import React from 'react'
+import Menu from '../template/Menu';
+import Todo from '../todo/Todo';
+import About from '../about/About';
 
 export default props => {
 
   return (
 
-    <div className="container">
-      <h1>Teste</h1>
+    <div className="">
+      <Menu />
+      <Todo />
+      <About />
     </div>
 
   );

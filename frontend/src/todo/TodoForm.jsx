b9c92ec6b1676afcd46dad5a8091ba58b9a830d6
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-14 14:14:10 -0300

    Form structure

--- frontend/src/todo/TodoForm.jsx
+++ frontend/src/todo/TodoForm.jsx
@@ -4,8 +4,15 @@ export default props => {
 
   return (
 
-    <div>
-      <h1>form</h1>
+    <div role='form' className="todoForm">
+      <div className="col-xs-12 col-sm-9 col-md-10">
+        <input type="text" id="description" className="form-control" placeholder="Add a task"/>
+      </div>
+      <div className="col-xs-12 col-sm-9 col-md-10">
+        <button className="btn btn-primary" value="">
+          <i className="fa fa-plus"></i>
+        </button>
+      </div>
     </div>
 
   );

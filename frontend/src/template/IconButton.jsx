b40ccd36c07592853c80b05fd3aafb2321ef97d0
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-14 14:48:52 -0300

    Created a If component conditional

--- frontend/src/template/IconButton.jsx
+++ frontend/src/template/IconButton.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import If from "./If";
 
 export default props => {
 
-  if(props.hide) {
+  return (
 
-    return null;
-
-  } else {
-    return (
+    <If test={!props.hide}>
 
       <button className={"btn btn-"+ props.style} onClick={props.onClick}>
         <i className={"fa fa-"+ props.icon}></i>
       </button>
+      
+    </If>
 
-    );
-  }
-
+  );
 }

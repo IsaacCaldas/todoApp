bc22fe78cf251ce0ab0f923e92f0e862cba78831
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-18 17:11:07 -0300

    Apply Middleware - Redux-Thunk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,10 +8,11 @@ import promise from 'redux-promise';
 
 // With redux-multi, you can put more 1 action an Action Creator
 import multi from 'redux-multi';
+import thunk from 'redux-thunk'
 
 import reducers from './main/reducers'
 
-const store = applyMiddleware(multi, promise)(createStore)(reducers);
+const store = applyMiddleware(thunk,multi, promise)(createStore)(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
